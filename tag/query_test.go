74616e3767bc785f1e814da103dbf03a5d889a46
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2024-02-15 03:31:33 +0700

    add more tag test cases.

--- tag/query_test.go
+++ tag/query_test.go
@@ -108,7 +108,7 @@ func (s *QueryTestSuite) TestReadPagePageWithSearch() {
 	s.Assert().Equal("Name 2", tags[1].Name)
 }
 
-func (s *QueryTestSuite) TestReadPagePageWithSearchFavoriteOnly() {
+func (s *QueryTestSuite) TestReadPageWithSearchFavoriteOnly() {
 	db, err := sql.Open("sqlite", "file:ent?mode=memory&_fk=1&_pragma=foreign_keys(1)")
 	s.Assert().Nil(err)
 	s.Assert().NotNil(db)
@@ -132,4 +132,79 @@ func (s *QueryTestSuite) TestReadPagePageWithSearchFavoriteOnly() {
 	s.Assert().Equal(1, len(tags))
 	s.Assert().Equal("Name 1", tags[0].Name)
 	s.Assert().Equal(true, tags[0].Favorite)
+}
+
+func (s *QueryTestSuite) TestCount() {
+	db, err := sql.Open("sqlite", "file:ent?mode=memory&_fk=1&_pragma=foreign_keys(1)")
+	s.Assert().Nil(err)
+	s.Assert().NotNil(db)
+	defer db.Close()
+
+	client := enttest.NewClient(s.T(), enttest.WithOptions(ent.Driver(dialect_sql.OpenDB("sqlite3", db))))
+	defer client.Close()
+
+	client.Tag.Create().SetName("Tag 1").SetFavorite(false).Save(context.Background())
+	client.Tag.Create().SetName("Tag 2").SetFavorite(false).Save(context.Background())
+	client.Tag.Create().SetName("Tag 3").SetFavorite(false).Save(context.Background())
+
+	c, err := Count(context.Background(), client,
+		QueryParams{
+			FavoriteOnly: false,
+			Search:       "",
+			Page:         0,
+			ItemPerPage:  30,
+		},
+	)
+
+	s.Assert().Nil(err)
+	s.Assert().Equal(3, c)
+}
+
+func (s *QueryTestSuite) TestCountPageWithSearch() {
+	db, err := sql.Open("sqlite", "file:ent?mode=memory&_fk=1&_pragma=foreign_keys(1)")
+	s.Assert().Nil(err)
+	s.Assert().NotNil(db)
+	defer db.Close()
+
+	client := enttest.NewClient(s.T(), enttest.WithOptions(ent.Driver(dialect_sql.OpenDB("sqlite3", db))))
+	defer client.Close()
+
+	client.Tag.Create().SetName("Name 1").SetFavorite(false).Save(context.Background())
+	client.Tag.Create().SetName("Name 2").SetFavorite(false).Save(context.Background())
+	client.Tag.Create().SetName("Tag 3").SetFavorite(false).Save(context.Background())
+
+	c, err := Count(context.Background(), client,
+		QueryParams{
+			FavoriteOnly: false,
+			Search:       "name",
+			Page:         0,
+			ItemPerPage:  30,
+		})
+
+	s.Assert().Nil(err)
+	s.Assert().Equal(2, c)
+}
+
+func (s *QueryTestSuite) TestCountWithSearchFavoriteOnly() {
+	db, err := sql.Open("sqlite", "file:ent?mode=memory&_fk=1&_pragma=foreign_keys(1)")
+	s.Assert().Nil(err)
+	s.Assert().NotNil(db)
+	defer db.Close()
+
+	client := enttest.NewClient(s.T(), enttest.WithOptions(ent.Driver(dialect_sql.OpenDB("sqlite3", db))))
+	defer client.Close()
+
+	client.Tag.Create().SetName("Name 1").SetFavorite(true).Save(context.Background())
+	client.Tag.Create().SetName("Name 2").SetFavorite(false).Save(context.Background())
+	client.Tag.Create().SetName("Tag 3").SetFavorite(false).Save(context.Background())
+
+	c, err := Count(context.Background(), client, QueryParams{
+		FavoriteOnly: true,
+		Search:       "name",
+		Page:         0,
+		ItemPerPage:  30,
+	})
+
+	s.Assert().Nil(err)
+	s.Assert().Equal(1, c)
 }
